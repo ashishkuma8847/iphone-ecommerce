979dd702929454a189de2031b57ce78254365e9b
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-01-17 12:46:09 +0530

    cc

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
-import bannerr from './Banner.json'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import bannerr from "./Banner.json";
+import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <>
-    <div className="container mt-[40px]">
-
-      <div className="bannerr w-[217px]">
-        {
-          bannerr.map((item, index) =>(
+      <div className="container mt-[40px]">
+       
+        <div className="bannerr w-[217px] ">
+          {bannerr.map((item, index) => (
             <div key={index}>
-              <div className=" flex">
-              <Link to={item.to}>
-              <div className=" flex w-[217px] justify-between font-customfont12 ">
-                <h1 className='font-normal text-base leading-[24px] '>{item.name}</h1>
-                <img src={item.img} alt="image for arrow" />
-                </div>
-              </Link>
-                
+              <div className=" flex pb-4">
+                <Link to={item.to}>
+                  <div className=" flex w-[217px] justify-between font-customfont12 ">
+                    <h1 className="font-normal text-base leading-[24px] ">
+                      {item.name}
+                    </h1>
+                    <img src={item.img} alt="image for arrow" />
+                  </div>
+                </Link>
               </div>
-              
             </div>
-          ))
-        }
-      </div>
+          ))}
         </div>
-      
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
